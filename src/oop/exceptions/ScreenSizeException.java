aa9968aac0d375d44ac3fd2f8acd5b8dee77f7da
Luka <luka.vinkesevic@fer.hr>
2019-06-17 21:09:29 +0200

    Delete ScreenSizeException.java

--- src/oop/exceptions/ScreenSizeException.java
+++
@@ -1,9 +0,0 @@
-package oop.exceptions;
-
-public class ScreenSizeException extends CustomException {
-	private static final long serialVersionUID = 1L;
-
-	public ScreenSizeException(String message) {
-		super("ScreenSizeException " + message);
-	}
-}
